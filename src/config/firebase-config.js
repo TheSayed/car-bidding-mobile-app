b931a6cc821cd56adcf4d2fa2fcf82a2bcab46f0
TheSayed <kotp9a@gmail.com>
2025-10-05 15:23:23 +0300

    Edit

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCDT-U-8fFyCwhOb06YsXoMD2RWWmfO3qM",
+  authDomain: "carbid-666f8.firebaseapp.com",
+  projectId: "carbid-666f8",
+  storageBucket: "carbid-666f8.firebasestorage.app",
+  messagingSenderId: "605805604155",
+  appId: "1:605805604155:web:66391c0ffdc674f072db1e",
 };
 
 const app = initializeApp(firebaseConfig);
